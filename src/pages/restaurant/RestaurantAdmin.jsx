697ef2c7f5ad01b1921174b23d200aaa4c2986ef
Anshika <anshikakoundal19004@gmail.com>
2026-03-13 14:50:33 +0530

    nothing

--- src/pages/restaurant/RestaurantAdmin.jsx
+++ src/pages/restaurant/RestaurantAdmin.jsx
@@ -14,6 +14,7 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
+
 const RestaurantAdmin = () => {
   const navigate = useNavigate();
   const [restaurantData, setRestaurantData] = useState(null);
